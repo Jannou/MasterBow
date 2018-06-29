eaa006c917ed1f21d07889368ea146b3d660679f
Jannou <jannoub@gmail.com>
2018-06-29 21:10:08 +0200

    add test for frame up project settings

---
+++ out/test/TestFrame.java
@@ -0,0 +1,50 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+
+class TestFrame{
+    private Frame testFrame;
+    private Frame testFrameStrike;
+
+    @BeforeEach
+    void setUp() {
+        testFrame = new Frame(7,1 );
+        testFrameStrike = new Frame();
+    }
+
+    @Test
+    void getType() {
+        Assertions.assertEquals(Frame.KindOfFrame.Strike,testFrameStrike.getType() );
+        Assertions.assertEquals(Frame.KindOfFrame.Regular,testFrame.getType() );
+        testFrame=new Frame(9,1 );
+        Assertions.assertEquals(Frame.KindOfFrame.Spare,testFrame.getType() );
+//        testFrame=new Frame(9,5); // => system -1 ok
+
+
+    }
+
+    @Test
+    void getScore() {
+        Assertions.assertEquals(10,testFrameStrike.getScore() );
+        Assertions.assertEquals(8,testFrame.getScore() );
+
+    }
+
+    @Test
+    void getNumberOfFallenPinByTheFirstBall() {
+        Assertions.assertEquals(10,testFrameStrike.getNumberOfFallenPinByTheFirstBall() );
+
+        Assertions.assertEquals(7,testFrame.getNumberOfFallenPinByTheFirstBall() );
+
+    }
+
+    @Test
+    void getNumberOfFallenPinByTheSecondBall() {
+
+        Assertions.assertEquals(0,testFrameStrike.getNumberOfFallenPinByTheSecondBall() );
+
+        Assertions.assertEquals(1,testFrame.getNumberOfFallenPinByTheSecondBall() );
+    }
+}
